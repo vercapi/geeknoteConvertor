07b5e10a4262b8a27d84a45ef687e2a6185de3b9
Pieterv <pieterv.sorano@gmail.com>
2014-05-07 20:30:43 +0200

    Fixed tests

--- testGeeknoteConvertor.py
+++ testGeeknoteConvertor.py
@@ -1,5 +1,5 @@
 import unittest
-import geeknoteConvertor
+import geeknoteConvertorLib
 
 class TestGeeknoteConvertor(unittest.TestCase):
     
@@ -7,54 +7,54 @@ class TestGeeknoteConvertor(unittest.TestCase):
         source = "* this is a bulletpoint"
         target = "# this is a bulletpoint"
 
-        result = geeknoteConvertor.replaceChar(source, "*", "#")
+        result = geeknoteConvertorLib.replaceChar(source, "*", "#")
         self.assertEqual(target, result)
 
     def test_replaceCharMutliple(self):
         source = "** this is a bulletpoint"
         target = "## this is a bulletpoint"
 
-        result = geeknoteConvertor.replaceChar(source, "*", "#")
+        result = geeknoteConvertorLib.replaceChar(source, "*", "#")
         self.assertEqual(target, result)
 
     def test_escapeChars(self):
         source = " # just something _ I'm writing"
         target = " \# just something \_ I'm writing"
         
-        result = geeknoteConvertor.escapeChars(source, ["#", "_"])
+        result = geeknoteConvertorLib.escapeChars(source, ["#", "_"])
         self.assertEqual(target, result)
 
     def test_escapeCharsMutliple(self):
         source = " # just something __ I'm writing"
         target = " \# just something \_\_ I'm writing"
         
-        result = geeknoteConvertor.escapeChars(source, ["#", "_"])
+        result = geeknoteConvertorLib.escapeChars(source, ["#", "_"])
         self.assertEqual(target, result)
 
     def test_escapeCharsFile(self):
         source = ["# test", "_ test"]
-        target = ["\# test", "\_ test"]
+        target = ["\\# test", "\\_ test"]
         
-        result = geeknoteConvertor.escapeCharsFile(source, ["#", "_"])
+        result = geeknoteConvertorLib.escapeCharsFile(source, ["#", "_"])
         self.assertEqual(target, result)
 
     def test_replaceCharFile(self):
         source = ["# test", "_ test"]
         target = ["* test", "_ test"]
         
-        result = geeknoteConvertor.replaceCharFile(source, "#", "*")
+        result = geeknoteConvertorLib.replaceCharFile(source, "#", "*")
         self.assertEqual(target, result)
 
     def test_replaceEscapedChar(self):
         source = "\\* this is a bulletpoint"
         target = "* this is a bulletpoint"
 
-        result = geeknoteConvertor.replaceChar(source, "\\*", "*")
+        result = geeknoteConvertorLib.replaceChar(source, "\\*", "*")
         self.assertEqual(target, result)
 
     def test_unsecapeChar(self):
         source = "\* this is a bulletpoint"
         target = "* this is a bulletpoint"
 
-        result = geeknoteConvertor.unescapeChars(source, ["*", "."])
+        result = geeknoteConvertorLib.unescapeChars(source, ["\*", "*"])
         self.assertEqual(target, result)
